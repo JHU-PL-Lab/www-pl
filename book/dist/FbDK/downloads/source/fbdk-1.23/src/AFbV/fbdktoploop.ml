a23912091dad5f4197c55d27ffd378b6d0f5b02c
scottfsmith <scott@cs.jhu.edu>
2016-01-20 16:00:34 -0500

    new book version

--- book/dist/FbDK/downloads/source/fbdk-1.23/src/AFbV/fbdktoploop.ml
+++ book/dist/FbDK/downloads/source/fbdk-1.23/src/AFbV/fbdktoploop.ml
@@ -10,13 +10,17 @@
 #load "afbvpp.cmo";;
 #load "afbvinterp.cmo";;
 
+(* uncomment the following line to turn the debug mode on *)
+
+(* Afbvoptions.set_debug(true);; *)
+
 (* Make some structs available at the top for easier use *)
 
 open Afbvast;;
 open Afbvinterp;;
 
 
-(* function parse parses FbST concrete syntax you enter as a string *)
+(* function parse parses AFbV concrete syntax you enter as a string *)
 
 let parse s = 
     let lexbuf = Lexing.from_string (s^";;") in
@@ -30,58 +34,13 @@ let pp e = print_string (Afbvpp.pretty_print e);;
 
 let ppeval x = print_string "==> ";pp (eval x);;
 
-(* function rep is a read-eval-print function for Fb programs: an interpreter *)
+(* function rep is a read-eval-print function for AFbV programs: an interpreter *)
 
 let rep s = ppeval (parse s);;
 
-(* 
-let s1 = "Let f = Function x -> x + 1 In f 2" ;; 
-*)
-let s1 = "Let x = `Negative (0-3) In Match x With `Negative x -> x | `Zero x -> 0 | `Positive x -> x" ;;  
+(* some actor examples *)
 
-let s2 = "
-Let ycomb = (Function body -> Let wrapper = Function this -> Function arg -> body (this this) arg In Function arg -> wrapper wrapper arg) In
-ycomb (Function this -> Function x -> If x = 0 Then 0 Else x + this (x - 1)) 4
-" ;;
-
-let s3 = "
-Let ycomb = (Function body -> Let wrapper = Function this -> Function arg -> body (this this) arg In Function arg -> wrapper wrapper arg) In
-Let f = Function myaddr -> ycomb (
-  Function this -> Function localdata -> Function msg ->
-    Match msg With
-      `main(n) -> (myaddr <- `count(n)); this(0)
-    | `count(n) -> (Print n) ; (If n = 0 Then this(0) Else myaddr <- `count(n-1)); this(0)
-)
-In
-Let a = Create(f, 0) In
-  a <- `main(5)
-" ;;
-
-let s4 = "
-Let ycomb = (Function body -> Let wrapper = Function this -> Function arg -> body (this this) arg In Function arg -> wrapper wrapper arg) In
-Let f = Function myaddr -> ycomb (
-  Function this -> Function localdata -> Function msg ->
-    Match msg With
-      | `count(n) -> (Print localdata) ; (If localdata = 0 Then this(0) Else myaddr <- `count(0)); this(localdata - 1)
-)
-In
-Let a = Create(f, 5) In
-  a <- `count(0)
-" ;;
-
-rep s4 ;;
-
-
-let s7 = "
-Let ycomb = (Function body -> Let wrapper = Function this -> Function arg -> body (this this) arg In Function arg -> wrapper wrapper arg) In
-Let reversem = Function f -> Function this -> Function arg -> f arg this In
-  ycomb (reversem (Function arg -> Function this ->
-           If arg = 0 Then 0 Else arg + this (arg - 1))) 5
-"
-;;
-rep s7 ;;
-
-let s7 = "
+let s1 = "
 Let ycomb = (Function body -> Let wrapper = Function this -> Function arg -> body (this this) arg In Function arg -> wrapper wrapper arg) In
 Let count = ycomb (Function this -> Function data -> Function msg ->
   Match msg With 
@@ -99,7 +58,50 @@ In
   Let a = Create(f, 0) In
       sendn a 3 (`count 0) ; ( a <- `up(False) ) ; sendn a 3 (`count 0) 
   ";;
-rep s7 ;;
+rep s1 ;;
 
-  
-  
+
+(* Counting down example *)
+
+let s2 = "
+Let y = (Function body -> Let wrapper = Function self -> Function msg -> 
+          body (self self) msg In wrapper wrapper) In
+Let actorBeh = Function me -> y (Function this -> Function data -> Function msg ->
+                                    Match msg With
+                                        `count(n) ->
+                                            (Print n);
+                                            (If n = 0 Then 0 Else (me <- (`count (n-1))));
+                                            this 0
+                               ) In
+Let actor = Create(actorBeh, 0) In
+	  actor <- `count 4";;
+
+rep s2;;
+
+(* ping pong example *)
+
+let s3 = "
+Let y = (Function body -> Let wrapper = Function self -> Function msg -> 
+          body (self self) msg In wrapper wrapper) In
+Let behPong = Function me -> y (Function this -> Function data -> Function msg ->
+                                    Match msg With
+                                        `pong(n) ->
+                                            (data <- (`ping (n+1))); (* invariant: data is a1 *)
+                                            this data (* Use the same behavior for the next message received *)
+                               ) In
+Let behPing = Function me -> Function dummy -> Function msg0 ->
+	 (* First message should be `init; create pong actor and get it going *)
+     Match msg0 With
+		    `init(n) -> Let a2 = Create(behPong, me) In (* tell ponger about me when its made *)
+						(a2 <- `pong(n)); (* send pong an n-ball to start the game *)
+							(* Now set behavior for rest of ping/pong game: get a ping, send a pong *)														   
+            (y (Function this -> Function msg ->
+               Match msg With `ping(n) ->
+                 (Print (n));
+                 (If n = 0 Then 0 Else (a2 <- (`pong (n-2))));
+                 this 
+            )) In
+Let a1 = Create(behPing, 0) In
+	  a1 <- `init(4)";;
+
+rep s3;;
