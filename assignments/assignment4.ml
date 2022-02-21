bf823460a407e8b754ac6afb68df0f1c88456ce2
Scott Smith <scott@cs.jhu.edu>
2022-02-21 09:36:43 -0500

    no message

--- assignments/assignment4.ml
+++ assignments/assignment4.ml
@@ -39,24 +39,24 @@ List of Collaborators :
    working for positive numbers first and then dealing with
    negatives.)  *)
 
-   let fbLt = "(0 1)";;
-   let fbMult = "(0 1)";;
-   let fbMod = "(0 1)";;
+let fb_lt = "(0 1)";;
+let fb_mult = "(0 1)";;
+let fb_mod = "(0 1)";;
    
-   (*
-   assert(peu ("("^fbLt^") 33 3") = "False");;
+(*
+   assert(peu ("("^fb_lt^") 33 3") = "False");;
       --> 33 < 3 => False
-   assert(peu ("("^fbLt^") (-1) 3") = "True");;
+   assert(peu ("("^fb_lt^") (-1) 3") = "True");;
       --> -1 < 3 => True
-   assert(peu ("("^fbMult^") 5 3") = "15");;
+   assert(peu ("("^fb_mult^") 5 3") = "15");;
       --> 5 * 3 => 15
-   assert(peu ("("^fbMult^") (-3) 5") = "-15");;
+   assert(peu ("("^fb_mult^") (-3) 5") = "-15");;
       --> (-3) * 5 => (-15)
-   assert(peu ("("^fbMod^") 33 3") = "0");;
+   assert(peu ("("^fb_mod^") 33 3") = "0");;
       --> 33 mod 3 => 0
-   assert(peu ("("^fbMod^") 87 4") = "3");;
+   assert(peu ("("^fb_mod^") 87 4") = "3");;
       --> 87 mod 4 => 3
-   *)
+*)
    
 (* Part 2 question 2.
 
@@ -66,52 +66,57 @@ List of Collaborators :
 
    Essentially this encoding allows us to represent booleans as functions. For example:
 
-         True --> Function t -> Function f -> t
-         False --> Function t -> Function f -> f
+         True --> Fun t -> Fun f -> t
+         False --> Fun t -> Fun f -> f
 
-   We will write 5 functions that work with church booleans in this section. Remember that all
-   your answers should generate Fb ASTs.
+   We will write five functions that work with church booleans in this section. 
+   Remember that all your answers should generate Fb programs as strings.
 
 *)
    
-   (* Write a Fb function to convert a church encoded bool to an Fb native bool.*)
-   let fbUnchurch = "(0 1)";;
+(* To start with let us make OCaml string versions of this Church encoding *)
+let church_true = "(Fun t -> Fun f -> t)";;
+let church_false = "(Fun t -> Fun f -> f)";;
+
+(* Write a Fb function to convert a church encoded bool to an Fb native bool.*)
+let fb_unchurch = "(0 1)";;
    
-   (* Write a Fb function to convert an Fb native bool to a church encoded bool *)
-   let fbChurch = "(0 1)";;
+(* Write a Fb function to convert an Fb native bool to a church encoded bool *)
+let fb_church = "(0 1)";;
    
-   (*
-   let churchTrue = "Function t -> Function f -> t";;
-   assert (peu ("("^fbUnchurch^")("^churchTrue^")") = "True" );;
-   *)
+(*
+   assert (peu fb_unchurch^"("^fb_church^"(True))" = "True" );;
+*)
    
-   (* Write a function to express simple IF THEN ELSE logic for church encoded bools *)
-   let fbChurchIf = "(0 1)";;
+(* Write a function to find out the logical NOT of a church encoded bool *)
+let fbchurch_not = "(0 1)";;
    
-   (* Write a function to find out the logical NOT of a church encoded bool *)
-   let fbChurchNot = "(0 1)";;
+(* Write a function to find out the logical AND of two church encoded bools *)
+let fbchurch_and = "(0 1)";;
    
-   (* Write a function to find out the logical AND of two church encoded bools *)
-   let fbChurchAnd = "(0 1)";;
+(* Write a function to find out the logical OR of two church encoded bools *)
+let fbchurch_or = "(0 1)";;
    
-   (* Write a function to find out the logical OR of two church encoded bools *)
-   let fbChurchOr = "(0 1)";;
+(* Write a function to find out the logical XOR of two church encoded bools *)
+let fbchurch_xor = "(0 1)";;
    
-   (* Write a function to find out the logical XOR of two church encoded bools *)
-   let fbChurchXOr = "(0 1)";;
-   
-   (*
-   let churchTrue = "(Function t -> Function f -> t)";;
-   let churchFalse = "(Function t -> Function f -> f)";;
-   assert(peu (fbUnchurch^"("^fbChurchNot^churchTrue^")") = "False" );; 
+(* Write a function which will accept an (endoded) boolean and two frozen 
+      Fb expressions and will thaw only the appropriate expression. 
+      (The reason for freezing the then/else code is if not they would both always run!
+      Think about it.)
+*)
+let fbchurch_if_then = "(0 1)";;
+      
+(*
+   assert(peu (fb_unchurch^"("^fbchurch_not^church_true^")") = "False" );; 
       --> Not True => False
-   assert(peu (fbUnchurch^"("^fbChurchIf^churchTrue^churchFalse^churchTrue^")") = "False" );; 
-      --> If True Then False Else True -> False
-   assert(peu (fbUnchurch^"("^fbChurchAnd^churchTrue^churchFalse^")") = "False" );;
+   assert(peu (fb_unchurch^"("^fbchurch_and^church_true^church_false^")") = "False" );;
       --> True And False => False
-   assert(peu (fbUnchurch^"("^fbChurchOr^churchTrue^churchFalse^")") = "True" );;
+   assert(peu (fb_unchurch^"("^fbchurch_or^church_true^church_false^")") = "True" );;
       --> True Or False => True
-   assert(peu (fbUnchurch^"("^fbChurchXOr^churchTrue^churchTrue^")") = "False" );;
-      --> True XOr True => False 
-   *)
+   assert(peu (fb_unchurch^"("^fbchurch_xor^church_true^church_true^")") = "False" );;
+      --> True XOr True => False    
+   assert(peu (fbchurch_if_then^church_true^"(Fun _ -> 4+1)(Fun _ -> 4-1)") = "5" );;
+      --> If True Then 4+1 Else 4-1 => 5 
+*)
    
